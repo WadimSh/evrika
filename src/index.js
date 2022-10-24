3cf525dcca0544a22b18ed5ec34e9b92ca6f0bbe
WadimSh <wadim11@yandex.ru>
2022-10-24 15:51:56 +0300

    fix: сверстан мобильный вариант header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './index.css';
+
+const popup = document.querySelector('.popup');
+const botton = document.querySelector('.header__hamburder-lines');
+const lineOne = document.querySelector('.line-one');
+const lineTwo = document.querySelector('.line-two');
+const lineThree = document.querySelector('.line-three');
+const item = document.querySelector('.popup__list');
+
+botton.addEventListener('click', () => {
+  lineOne.classList.toggle('line-one-rotate');
+  lineTwo.classList.toggle('line-two-rotate');
+  lineThree.classList.toggle('line-three-rotate');
+  popup.classList.toggle('active');
+});
+
+item.addEventListener('click', () => {
+  lineOne.classList.remove('line-one-rotate');
+  lineTwo.classList.remove('line-two-rotate');
+  lineThree.classList.remove('line-three-rotate');
+  popup.classList.remove('active');
+});
